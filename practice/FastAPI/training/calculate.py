90c2e8d80c7203072e0ae3300f7017a71385bab5
greedjar74 <hong7498kim@navaer.com>
2024-07-25 21:53:49 +0900

    mnist autoencoder 연습, fastapi와 streamlit을 활용한 간단한 예제

---
+++ practice/FastAPI/training/calculate.py
@@ -0,0 +1,9 @@
+def calculate(operation, x, y):
+    if operation == 'add':
+        return x + y
+    elif operation == 'sub':
+        return x - y
+    elif operation == 'mul':
+        return x * y
+    else :
+        return x / y
