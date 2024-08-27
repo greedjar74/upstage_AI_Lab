cb2b500f40f78ddcbbadc49bbda9055e4a9fc365
greedjar74 <hong7498kim@navaer.com>
2024-08-27 17:09:25 +0900

    RNN, LSTM, GRU 실습

--- practice/FastAPI/training/calculate.py
+++
@@ -1,9 +0,0 @@
-def calculate(operation, x, y):
-    if operation == 'add':
-        return x + y
-    elif operation == 'sub':
-        return x - y
-    elif operation == 'mul':
-        return x * y
-    else :
-        return x / y
